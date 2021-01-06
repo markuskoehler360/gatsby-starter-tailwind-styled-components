f38450916f93f4686d74ece316ed23bc087c456f
markuskoehler360 <markusgosling@gmail.com>
2021-01-06 23:41:42 +0100

    add gatsby-plugin-manifest and custom favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,20 @@
 module.exports = {
-  siteMetadata: {
-    title: "gatsby-starter-tailwind-styled-components",
-  },
-  plugins: ["gatsby-plugin-styled-components"],
+    siteMetadata: {
+        title: "gatsby-starter-tailwind-styled-components",
+    },
+    plugins: [
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-tailwind-styled-components`,
+                short_name: `gatsby-tw-sc`,
+                start_url: `/`,
+                background_color: `#f7f0eb`,
+                theme_color: `#a2466c`,
+                display: `standalone`,
+                icon: `src/images/favicon.png`,
+            },
+        },
+        "gatsby-plugin-styled-components",
+    ],
 };
